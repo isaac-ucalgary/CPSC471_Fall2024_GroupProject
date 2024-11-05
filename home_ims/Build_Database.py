5ae4f326654063284f6e020c534ff9031a6dfcbf
isaacst <isaac.shiellsthomas@ucalgary.ca>
2024-11-05 16:49:10 -0700

    Clean up

--- home_ims/Build_Database.py
+++ home_ims/Build_Database.py
@@ -4,8 +4,11 @@ import mysql.connector
 from mysql.connector import Error
 from sql_statements import *
 
+
+
+
 # Connect to database
-def connect(db_name=str):
+def connect(db_name:str):
     connection = None
     try:
         # Not sure if this is correct...
@@ -24,43 +27,54 @@ def connect(db_name=str):
     
     return connection
 
+
+
+
 # Create database
 # Change MariaDB database to be stored in project folder
-def create_db(connection, db_name=str):
+def create_db(connection, db_name:str):
     cursor = connection.cursor()
     try:
-        cursor.execute("CREATE DATABASE %s;", db_name)
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name};")
     except Error as e:
         print(e)
 
+
+
 # Execute an SQL statement on the connected database
 # I think this function would work for all SQL queries?
-def exec_sql(connection, sql_statement=str):
+def exec_sql(connection, sql_statement:str):
     cursor = connection.cursor()
     try:
         cursor.execute(sql_statement)
     except Error as e:
         print(e)
 
-# Main
-if __name__ == "__main__":
-    db_name = "HomeIMS"
-    connection = connect()
+
+
+
+def build_database():
+    db_name:str = "HomeIMS"
+    connection = connect(db_name)
     
     if connection:
         create_db(connection, db_name)
-        connection.close()
 
-        connection = connect(db_name)
-        if connection:
-            # Loop through the SQL statements for creating tables
-            for i in sql_tables:
-                exec_sql(connection, sql_tables[i])
-                # Print statements for debugging
-                command = sql_tables.split()[:3]
-                print(f"Success:    {command}")
-            connection.close()
+        # Loop through the SQL statements for creating tables
+        for action in sql_tables:
+            exec_sql(connection, action)
+            # Print statements for debugging
+            command = action.split()[:3]
+            print(f"Success:    {command}")
+
+        connection.close()
 
     else:
         print(f"Connection failed, database {db_name} not created.")
+
+
+# Main
+if __name__ == "__main__":
+    build_database()
+
 
