1b093cc097aad7e823cbe369fa43aebf455aee3c
Isaac <isaac.rst@proton.me>
2024-11-05 22:52:25 -0700

    Created python env file and converted Build_Database.py to OOP

--- home_ims/src/Build_Database.py
+++ home_ims/src/Build_Database.py
@@ -1,80 +1,169 @@
 # Build Database Script
 
+# -- Library Imports --
 import mysql.connector
 from mysql.connector import Error
+
+# -- Local Imports --
+from env import MARIADB_HOST, MARIADB_PORT, MARIADB_DATABASE_NAME, MARIADB_USER
+from secrets import MARIADB_PASSWORD
 from sql_statements import *
 
 
 
+class Build_Database:
 
-# Connect to database
-def connect(db_name:str):
-    connection = None
-    try:
-        # Not sure if this is correct...
-        connection = mysql.connector.connect(
-            host='localhost', # I think we are using local host?
-            user='TBD',
-            password='TBD',
-            database=db_name
-        )
-        if db_name:
-            print(f"Connected to \"{db_name}\".")
+
+    def __init__(self,
+                 db_host:str=MARIADB_HOST,
+                 db_port:int=MARIADB_PORT,
+                 db_user:str=MARIADB_USER,
+                 db_password:str=MARIADB_PASSWORD,
+                 db_name:str=MARIADB_DATABASE_NAME
+                 ):
+
+
+        self.db_host = db_host
+        self.db_port = db_port
+        self.db_user = db_user
+        self.db_password = db_password
+        self.db_name = db_name
+
+        self.__connection = None;
+        self.__cursor = None;
+
+
+
+
+
+    # Connect to database
+    # Creates a connection and a cursor
+    def __connect(self) -> bool:
+
+        connection_success:bool = True
+
+        try:
+            self.__connection = mysql.connector.connect(
+                host=self.db_host,
+                port=self.db_port,
+                user=self.db_user,
+                password=self.db_password,
+                database=self.db_name
+            )
+
+        except Error as e:
+            print("Failed to connect to database")
+            print(e)
+            self.__connection = None
+            connection_success = False
+
         else:
-            print("Connected to MariaDB.")
-    except Error as e:
-        print(e)
-    
-    return connection
+            if self.db_name:
+                print(f"Connected to \"{self.db_name}\".")
+            else:
+                print("Connected to MariaDB.")
+
+            self.__cursor = self.__connection.cursor()
+
+        return connection_success
+
+
+        
+
+    # Closes cursor and connection to the database.
+    def __close_connection(self) -> None:
+        if self.__cursor is not None:
+            self.__cursor.close()
+
+        if self.__connection is not None:
+            self.__connection.close()
 
 
 
 
-# Create database
-# Change MariaDB database to be stored in project folder
-def create_db(connection, db_name:str):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name};")
-    except Error as e:
-        print(e)
+    # Create database
+    def __create_db(self) -> bool:
+
+        # Nothing has failed yet
+        operation_successful:bool = True
+
+        # Try to create the database
+        if self.__cursor is not None:
+            try:
+                self.__cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.db_name};")
+            except Error as e:
+                print(e)
+                operation_successful = False # Record failure
+        
+        else:
+            operation_successful = False # Record failure 
+            if self.__connection is None:
+                print("Could not create database. Connection and cursor not established.")
+            else:
+                print("Could not create database. Connection not established.")
+
+        # Return success status
+        return operation_successful
 
 
 
-# Execute an SQL statement on the connected database
-# I think this function would work for all SQL queries?
-def exec_sql(connection, sql_statement:str):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(sql_statement)
-    except Error as e:
-        print(e)
+
+    # Execute an SQL statement on the connected database
+    # NOTE I think this function would work for all SQL queries?
+    # NOTE We may want this to be psydo public
+    def __exec_sql(self, sql_statement) -> bool:
+
+        # Nothing has failed yet
+        operation_successful:bool = True
+
+        if self.__cursor is not None:
+            try:
+                self.__cursor.execute(sql_statement)
+            except Error as e:
+                print(e)
+                operation_successful = False # Record failure
+
+        else:
+            operation_successful = False # Record failure
+            if self.__connection is None:
+                print("Could not execute SQL statement. Connection and cursor not established.")
+            else:
+                print("Could not execute SQL statement. Connection not established.")
+
+
+        # Return success status
+        return operation_successful
 
 
 
+    def build_database(self) -> bool:
 
-def build_database():
-    db_name:str = "HomeIMS"
-    connection = connect(db_name)
-    
-    if connection:
-        create_db(connection, db_name)
+        # Nothing has failed yet
+        operation_successful:bool = True
 
-        # Loop through the SQL statements for creating tables
-        for action in sql_tables:
-            exec_sql(connection, action)
-            # Print statements for debugging
-            command = action.split()[:3]
-            print(f"Success:    {command}")
+        # Connect to the database
+        operation_successful = self.__connect() and operation_successful
 
-        connection.close()
+        # Create the database and tables.
+        if self.__connection:
+            operation_successful = self.__create_db() and operation_successful
 
-    else:
-        print(f"Connection failed, database {db_name} not created.")
+            # Loop through the SQL statements for creating tables
+            for statement in sql_tables:
+                operation_successful = self.__exec_sql(statement) and operation_successful
+                # Print statements for debugging
+                print(f"Success: {statement.split()[:3]}") # TODO Implement proper logging using the logging library
+
+        else:
+            operation_successful = False
+            print(f"Connection failed, database {self.db_name} not created.")
+
+        # Close the connection to the database
+        self.__close_connection()
+
+        # Return the status of building the database
+        return operation_successful
 
 
-# Main
-if __name__ == "__main__":
-    build_database()
 
 
--- home_ims/src/env.py
+++ home_ims/src/env.py
@@ -1,3 +1,4 @@
-MARIADB_HOST = "mariadb.homeims.praytomurphy.rodeo"
-MARIADB_PORT = 46467 # HMIMS from HoMeIMS in DTMF encoding
-MARIADB_USER = "Home_IMS_app"
+MARIADB_HOST:str = "mariadb.homeims.praytomurphy.rodeo"
+MARIADB_PORT:int = 46467 # HMIMS from HoMeIMS in DTMF encoding
+MARIADB_DATABASE_NAME:str = "Home_IMS"
+MARIADB_USER:str = "Home_IMS_app"
