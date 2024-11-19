d213cc5d1d97a092f9ce46e17011d2203525b6db
ErrBrush <errbrushed@gmail.com>
2024-11-19 16:01:14 -0700

    Begin implementing UI. Run `python .` within the `src` directory.

--- home_ims/src/__main__.py
+++ home_ims/src/__main__.py
@@ -2,10 +2,14 @@ import sys
 
 from PyQt6 import QtWidgets, uic
 
+def click():
+    print("Hello, world!")
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
 
-    window = uic.loadUi("/home/daniel/Downloads/MainWindow.ui")
+    window = uic.loadUi("mainwindow.ui")
+    window.pushButton.pressed.connect(click)
     window.show()
     app.exec()
 
