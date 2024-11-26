ea0eb425d9a9ce3b7b39cffa5244b628c3503678
ErrBrush <errbrushed@gmail.com>
2024-11-25 19:50:06 -0700

    Inventory view layout work.

--- home_ims/src/__main__.py
+++ home_ims/src/__main__.py
@@ -2,14 +2,21 @@ import sys
 
 from PyQt6 import QtWidgets, uic
 
-def click():
-    print("Hello, world!")
-
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
 
-    window = uic.loadUi("main.ui")
-    window.shoppingListButton.pressed.connect(click)
+    window = uic.loadUi("ui/main.ui")
+
+    gen, base = uic.loadUiType("ui/inv_entry.ui")
+
+    for i in range(30):
+        widget = base()
+        form = gen()
+        form.setupUi(widget)
+        form.quantity.setText(f"{i} mL")
+        window.inventoryEntries.layout().addWidget(widget)
+
+    window.inventoryEntries.layout().addStretch()
+
     window.show()
     app.exec()
-
